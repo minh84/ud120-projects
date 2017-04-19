c8f50691c7e4210e1813fd4e5437cf78045a9136
Minh VU <ngoc.vu@polytechnique.org>
2017-04-19 14:45:50 +0100

    finish validation

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,29 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+features_train, features_test, labels_train, labels_test = train_test_split(features,
+                                                                            labels,
+                                                                            test_size=0.3,
+                                                                            random_state=42)
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print (np.sum(labels_test))
+print (len(labels_test))
+print (accuracy_score(clf.predict(features_test), labels_test))
+print (precision_score(clf.predict(features_test), labels_test))
+print (recall_score(clf.predict(features_test), labels_test))
+print (np.where(np.array(labels_test)==1.0))
+print (np.where(np.array(clf.predict(features_test))==1.0))
+
+preds = np.array([0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1])
+truth = np.array([0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0])
+
+print (len(np.where((preds == 0) & (truth==1))[0]))
+# prec
+print (precision_score(truth, preds))
+print (recall_score(truth, preds))
